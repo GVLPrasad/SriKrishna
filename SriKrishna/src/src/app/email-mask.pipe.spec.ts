3c2a2b40cf2628a61cb10e87d0829c077d55344b
GVLPrasad <prasadgvl@gmail.com>
2023-02-12 19:22:17 +0530

    src

--- SriKrishna/src/src/app/email-mask.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EmailMaskPipe } from './email-mask.pipe';
-
-describe('EmailMaskPipe', () => {
-  it('create an instance', () => {
-    const pipe = new EmailMaskPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- SriKrishna/src/src/app/mobile-mask.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MobileMaskPipe } from './mobile-mask.pipe';
-
-describe('MobileMaskPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MobileMaskPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
